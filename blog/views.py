5c05a6ac0312b5b6b0f80437e50b82b125928ef0
rohsikdak <rohsikdak@naver.com>
2018-07-10 21:23:31 +0900

    로그인 임시구현

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from django.utils import timezone
-from .forms import PostForm
-from django.shortcuts import redirect
+from .forms import PostForm, UserForm, LoginForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
+from django.template import RequestContext
+from django.http import HttpResponse
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -39,4 +42,28 @@ def post_edit(request, pk):
         form = PostForm(instance = post)
     return render(request, 'blog/post_edit.html', {'form':form})
 
-# Create your views here.
+def sign_up(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            new_user = User.objects.create_user(**form.cleaned_data)
+            login(request, new_user)
+            return redirect('/')
+    else:
+        form = UserForm()
+    return render(request, 'blog/add_user.html', {'form' : form})
+
+def sign_in(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username = username, password = password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            return HttpResponse('로그인 실패. 다시시도 해보세요.')
+    else:
+        form = LoginForm()
+        return render(request, 'blog/login.html', {'form':form})
